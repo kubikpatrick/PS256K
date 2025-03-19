64ca00e1e36d4ce1c572bca98c106ae8e6744623
belmo <belmokhtarpatrick@gmail.com>
2025-03-19 10:02:49 +0100

    feat: add migrations

---
+++ Migrations/20250319083120_RenameHashPropertyToPathOfAlbumEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PS256K.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameHashPropertyToPathOfAlbumEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Hash",
+                table: "Medias",
+                newName: "Path");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "Medias",
+                newName: "Hash");
+        }
+    }
+}
