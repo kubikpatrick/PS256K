e5643f5d9a09fe126119766a70a872d04803d898
belmo <belmokhtarpatrick@gmail.com>
2025-03-19 22:41:00 +0100

    feat: create account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using PS256K.Data;
+using PS256K.Models.Identity;
+
+namespace PS256K.Controllers;
+
+[Authorize]
+[Route("account")]
+public sealed class AccountController : Controller
+{   
+    private readonly ApplicationDbContext _context;
+    private readonly SignInManager<User> _signInManager;
+    private readonly UserManager<User> _userManager;
+
+    public AccountController(ApplicationDbContext context, SignInManager<User> signInManager, UserManager<User> userManager)
+    {
+        _context = context;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    private User CurrentUser { get; set; }
+
+    [HttpGet]
+    public async Task<ActionResult> Index()
+    {
+        var user = await _context.Users
+            .Include(u => u.Albums)
+                .ThenInclude(a => a.Medias) 
+            .FirstOrDefaultAsync(u => u.Id == _userManager.GetUserId(User));
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        CurrentUser = await _userManager.GetUserAsync(User);
+
+        await base.OnActionExecutionAsync(context, next);
+    }
+}
