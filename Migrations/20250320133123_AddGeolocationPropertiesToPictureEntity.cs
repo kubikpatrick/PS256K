073811fddc066289643d97865965f5550ad2375e
belmo <belmokhtarpatrick@gmail.com>
2025-03-20 14:32:28 +0100

    feat: add migrations

---
+++ Migrations/20250320133123_AddGeolocationPropertiesToPictureEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PS256K.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeolocationPropertiesToPictureEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Latitude",
+                table: "Pictures",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Longitude",
+                table: "Pictures",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Pictures");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Pictures");
+        }
+    }
+}
