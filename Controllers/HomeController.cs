2c0743e5ea7d58e6a167bea491ea90e4ac0bd8b7
belmo <belmokhtarpatrick@gmail.com>
2025-03-20 10:28:18 +0100

    style: add carriage return

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,9 @@ public sealed class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public ActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel 
+        { 
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+        });
     }
 }
