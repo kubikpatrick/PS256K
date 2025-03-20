39f4a6f53d77b85e8853f8bac1e3ab6f92aaf608
belmo <belmokhtarpatrick@gmail.com>
2025-03-20 17:50:45 +0100

    style: remove useless `using`

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 
 using PS256K.Data;
