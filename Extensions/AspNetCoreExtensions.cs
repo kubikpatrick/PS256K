05c0cd8e70c1df4b75b45fa713a6d287a90de449
belmo <belmokhtarpatrick@gmail.com>
2025-03-23 17:40:02 +0100

    feat: add ASP.NET Core related classes extensions

---
+++ Extensions/AspNetCoreExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PS256K.Data;
+
+namespace PS256K.Extensions;
+
+public static class AspNetCoreExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        var context = app.Services.GetRequiredService<ApplicationDbContext>();
+        var migrations = context.Database.GetPendingMigrations();
+
+        if (!migrations.Any())
+        {
+            return;
+        }
+
+        context.Database.Migrate();
+    }
+
+    public static void CreateDatabase(this WebApplication app)
+    {
+        if (!File.Exists("./PS256K.db"))
+        {
+            File.Create("./PS256K.db");
+        }
+    }
+}
